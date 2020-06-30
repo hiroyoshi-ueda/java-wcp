c49371e7eae2999e293e95848dfb50cfda2d3bfa
上田寛祥 <uedahiroyoshi@uedahiroshishounoMacBook-Air.local>
2020-06-30 18:49:01 +0900

    Java 演習

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUKY_NUMBER= 7;
+
+		String str1;
+		str1 = "上田　寛祥";
+		System.out.println(str1);
+
+		str1= "七転び八起き";
+		System.out.println(str1);
+
+		System.out.println(LUKY_NUMBER);
+	}
+
+}
